7cc06d0dae131aecb8d8374e0d1b0e0d0c0e0e66
Анна Хагнес <Anna@MacBook-Air-Anna.local>
2022-09-13 21:38:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    person: null
+  }
+
+  async componentDidMount() {
+    
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    //получаем доступ к нашей информации
+    this.setState({person: data.results[0]})
+    //пишем results[0], т.к. когда смотрим в console, то видим, что вся инфо находится как в яйце results -> 0 -> и т.д.
+    console.log(data.results[0])
+  }
+  
+//используем тернарный оператор, т.к. render у нас загружается сразу и если мы пропишем 
+//<p>{this.state.person.name.first} </p>, то будет выдавать ошибку
+// тернарный оператор будет говорить нам, что если еще не получена инфо, то покажи нам Loading...
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!this.state.person ? <p>Loading...</p> : 
+      <div> 
+        <p>First Name: {this.state.person.name.first} </p>
+        <p>Last Name: {this.state.person.name.last} </p>
+        <p>Email: {this.state.person.email} </p>
+        <img src={this.state.person.picture.large} /> 
+      </div>
+      }
     </div>
   );
+}
 }
 
 export default App;
