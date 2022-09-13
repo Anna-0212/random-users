b95cb7c48c534f3dcd3a1f94715085a54d05d170
Anna-0212 <112332240+Anna-0212@users.noreply.github.com>
2022-09-13 21:40:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,10 @@ class App extends Component {
     const url = "https://api.randomuser.me/";
     const response = await fetch(url);
     const data = await response.json();
-
-    //получаем доступ к нашей информации
     this.setState({person: data.results[0]})
-    //пишем results[0], т.к. когда смотрим в console, то видим, что вся инфо находится как в яйце results -> 0 -> и т.д.
     console.log(data.results[0])
   }
   
-//используем тернарный оператор, т.к. render у нас загружается сразу и если мы пропишем 
-//<p>{this.state.person.name.first} </p>, то будет выдавать ошибку
-// тернарный оператор будет говорить нам, что если еще не получена инфо, то покажи нам Loading...
   render() {
   return (
     <div className="App">
